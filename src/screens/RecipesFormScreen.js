c86b802cc092c43e95ac29dcff28cbb1806240af
Fleabyte26 <krishornung@gmail.com>
2026-05-23 11:05:32 -0400

    Add FavoriteScreen FlatList, favoritesSlice reducer, RecipesFormScreen saverecipe function

--- src/screens/RecipesFormScreen.js
+++ src/screens/RecipesFormScreen.js
@@ -12,7 +12,25 @@ export default function RecipesFormScreen({ route, navigation }) {
   );
 
   const saverecipe = async () => {
- 
+    try {
+      const newrecipe = { title, image, description };
+
+      const storedRecipes = await AsyncStorage.getItem("customrecipes");
+      const recipes = storedRecipes ? JSON.parse(storedRecipes) : [];
+
+      if (recipeToEdit !== undefined) {
+        recipes[recipeIndex] = newrecipe;
+        await AsyncStorage.setItem("customrecipes", JSON.stringify(recipes));
+        if (onrecipeEdited) onrecipeEdited();
+      } else {
+        recipes.push(newrecipe);
+        await AsyncStorage.setItem("customrecipes", JSON.stringify(recipes));
+      }
+
+      navigation.goBack();
+    } catch (error) {
+      console.log("Error saving recipe:", error);
+    }
   };
 
   return (
